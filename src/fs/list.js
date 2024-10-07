a307f09c6e32900c62ff43f3940814fb30159cb0
Dramen <vvpalchin@gmail.com>
2024-10-07 21:25:30 +0300

    feat: implement File system: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { join } from "path";
+import { readdir } from "fs/promises";
+import { FsException, getFileDirName, targetExists } from "../helpers/index.js";
+
+const { __dirname } = getFileDirName(import.meta);
+
+const SOURCE_FOLDER = 'files';
+const SOURCE_FOLDER_PATH = join(__dirname, SOURCE_FOLDER);
+
 const list = async () => {
-    // Write your code here 
+  const isSourceFolderExist = await targetExists(SOURCE_FOLDER_PATH);
+
+  if (!isSourceFolderExist) {
+    throw new FsException();
+  }
+
+  const files = await readdir(SOURCE_FOLDER_PATH);
+
+  console.table(files)
 };
 
 await list();
