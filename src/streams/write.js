e1f127e080d178b706ed31ca4adcceb39715377d
Dramen <vvpalchin@gmail.com>
2024-10-07 23:23:43 +0300

    feat: implement Streams: write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'node:stream/promises';
+import { getFileDirName } from '../helpers/index.js';
+
+const { stdin, stdout } = process;
+const { __dirname } = getFileDirName(import.meta);
+
+const SOURCE_FOLDER = 'files';
+const SOURCE_FOLDER_PATH = join(__dirname, SOURCE_FOLDER);
+const DESTINATION_FILE = 'fileToWrite.txt';
+const DESTINATION_FILE_PATH = join(SOURCE_FOLDER_PATH, DESTINATION_FILE);
+
 const write = async () => {
-    // Write your code here 
+  stdout.write('Type the text below and press Enter:\n');
+  await pipeline(stdin, createWriteStream(DESTINATION_FILE_PATH));
 };
 
 await write();
