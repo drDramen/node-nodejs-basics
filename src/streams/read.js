290acd67ec0c0eb13f5ed83dde6cac3d3db22de6
Dramen <vvpalchin@gmail.com>
2024-10-07 23:20:19 +0300

    feat: implement Streams: read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { join } from 'path';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { getFileDirName } from '../helpers/index.js';
+
+const { stdout } = process;
+const { __dirname } = getFileDirName(import.meta);
+
+const SOURCE_FOLDER = 'files';
+const SOURCE_FOLDER_PATH = join(__dirname, SOURCE_FOLDER);
+const SOURCE_FILE = 'fileToRead.txt';
+const SOURCE_FILE_PATH = join(SOURCE_FOLDER_PATH, SOURCE_FILE);
+
 const read = async () => {
-    // Write your code here 
+  await pipeline(createReadStream(SOURCE_FILE_PATH), stdout);
 };
 
 await read();
