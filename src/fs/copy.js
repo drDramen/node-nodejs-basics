2ada0aee5e92c3da42829b5f5ff478b49c29a854
Dramen <vvpalchin@gmail.com>
2024-10-07 21:03:12 +0300

    feat: implement File system: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import { copyFile, mkdir, readdir } from 'fs/promises';
+import { join } from 'path';
+import { FsException, getFileDirName, targetExists } from '../helpers/index.js';
+
+const { __dirname } = getFileDirName(import.meta);
+
+const SOURCE_FOLDER = 'files';
+const SOURCE_FOLDER_PATH = join(__dirname, SOURCE_FOLDER);
+const DESTINATION_FOLDER = 'files_copy';
+const DESTINATION_FOLDER_PATH = join(__dirname, DESTINATION_FOLDER);
+
 const copy = async () => {
-    // Write your code here 
+  const isSourceFolderExist = await targetExists(SOURCE_FOLDER_PATH);
+  const isDestinationFolderExist = await targetExists(DESTINATION_FOLDER_PATH);
+
+  if (!isSourceFolderExist || isDestinationFolderExist) {
+    throw new FsException();
+  }
+
+  await mkdir(DESTINATION_FOLDER_PATH);
+  const files = await readdir(SOURCE_FOLDER_PATH);
+
+  await Promise.all(
+      files.map(async (file) => {
+        const sourceFilePath = join(SOURCE_FOLDER_PATH, file);
+        const destinationFilePath = join(DESTINATION_FOLDER_PATH, file);
+        return copyFile(sourceFilePath, destinationFilePath);
+      })
+  );
 };
 
 await copy();
