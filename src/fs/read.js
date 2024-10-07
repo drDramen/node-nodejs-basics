ab98180dab850a28d8f07e09dd220a74b76dba84
Dramen <vvpalchin@gmail.com>
2024-10-07 21:36:09 +0300

    feat: implement File system: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { join } from 'path';
+import { readFile } from 'fs/promises';
+import { FsException, getFileDirName, targetExists } from '../helpers/index.js';
+
+const { __dirname } = getFileDirName(import.meta);
+
+const SOURCE_FOLDER = 'files';
+const SOURCE_FOLDER_PATH = join(__dirname, SOURCE_FOLDER);
+const SOURCE_FILE = 'fileToRead.txt';
+const SOURCE_FILE_PATH = join(SOURCE_FOLDER_PATH, SOURCE_FILE);
+
 const read = async () => {
-    // Write your code here 
+  const isSourceFileExist = await targetExists(SOURCE_FILE_PATH);
+
+  if (!isSourceFileExist) {
+    throw new FsException();
+  }
+
+  const contents = await readFile(SOURCE_FILE_PATH, { encoding: 'utf8' });
+
+  console.log(contents);
 };
 
 await read();
